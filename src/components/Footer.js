40ce8db6aa70ecd8dde6b403e2eafc73e52191ba
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-05-13 16:14:37 -0700

    added about content, added footer content, removed email button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="footer">
-            <p><small>John Doe &copy; 2022</small></p>
+            <p><small>Damon Pickett &copy; 2022</small></p>
         </footer>
     );
 }
