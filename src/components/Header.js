6e6ffb9ce17c5c874a9fa40d04c5b5edca6884b0
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-05-13 13:34:45 -0700

    added profile pic and changed name on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 // Import Assets
-import profile from '../assets/profile.png';
+import profile from '../assets/profile-pic (2).jpg';
 
 const Header = () => {
     return (
         <section className='header'>
-            <img src={profile} alt="John Doe" />
+            <img src={profile} alt="Damon Pickett" />
 
             <div className='header__content'>
-                <h1>Hi, I'm John Doe</h1>
+                <h1>Hi, I'm <span className='camelCase'>damonPickett</span></h1>
                 <p>Blockchain Developer</p>
                 <button className='button'>Get In Touch</button>
             </div>
