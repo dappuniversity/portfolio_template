2225543fd923a466d6d90ae27169550d29986b44
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-06-07 11:48:50 -0700

    edited about me section, re-added max-width to app

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,6 @@ const About = () => {
       <h2>About Me</h2>
 
       <p>Hi, my name is Damon and I bring over a decade’s worth of professional experience in customer service, effective communication, and management. As a Blockchain Developer I want to build a world in which opportunity transcends national and institutional borders. I want to make it easy and secure for people all over the world to transact, acquire assets, and prosper. Using React.js and Solidity, I specialize in building user interfaces that interact with smart contracts. I am currently open to new opportunities in the blockchain, web3, and crypto industries.</p>
-      
     </section>
   );
 };
