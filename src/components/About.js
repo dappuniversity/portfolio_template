eeb415546608c566bda9a78d297402a285686ada
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-08-12 14:29:10 -0700

    updated About content

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
     <section className="about">
       <h2>About Me</h2>
 
-      <p>Hi, my name is Damon and I bring over a decade’s worth of professional experience in customer service, effective communication, and management. As a Blockchain Developer I want to build a world in which opportunity transcends national and institutional borders. I want to make it easy and secure for people all over the world to transact, acquire assets, and prosper. Using React.js and Solidity, I specialize in building user interfaces that interact with smart contracts. I am currently open to new opportunities in the blockchain, web3, and crypto industries.</p>
+      <p>I bring over a decade’s worth of professional experience in customer service, effective communication, and management. As a Blockchain Developer I want to build a world in which opportunity transcends national and institutional borders. I want to make it easy and secure for people all over the world to transact, acquire assets, and prosper. Using React.js and Solidity, I specialize in building user interfaces that interact with smart contracts. I am currently open to new opportunities in the blockchain, web3, and crypto industries.</p>
     </section>
   );
 };
